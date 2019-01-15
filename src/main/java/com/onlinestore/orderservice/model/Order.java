63c519ed109595831049cfc6341727c333772431
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-15 17:39:42 +0100

    added Order

--- src/main/java/com/onlinestore/orderservice/model/Order.java
+++ src/main/java/com/onlinestore/orderservice/model/Order.java
@@ -1,6 +1,17 @@
 package com.onlinestore.orderservice.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Order {
 	private String orderId;
+	private String customerId;
+	private Date orderDate;
+	private Date shippingDate;
+	private String orderStatus;
+	private BigDecimal orderTotal;
+
+	public Order() {
+	}
 
 }
