64a7381dc65181b21e5831508a7789aae8d68778
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-16 15:10:37 +0100

    added Customer

---
+++ src/main/java/com/onlinestore/orderservice/model/Customer.java
@@ -0,0 +1,7 @@
+package com.onlinestore.orderservice.model;
+
+public class Customer {
+	private int customerId;
+	private String customerName;
+	private String customerAddress;
+}
--- src/main/java/com/onlinestore/orderservice/model/Order.java
+++ src/main/java/com/onlinestore/orderservice/model/Order.java
@@ -3,14 +3,13 @@ package com.onlinestore.orderservice.model;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class Order {
 	private String orderId;
 	private String customerId;
 	private Date orderDate;
-	private Date shippingDate;
 	private String orderStatus;
-	private boolean paid;
-
+	private List<ShoppingPosition> shoppingPositions;
 }
