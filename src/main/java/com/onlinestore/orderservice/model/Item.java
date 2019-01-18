f8be2c00ff1464b95acfdaecf95ba7bd67ea5003
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-18 17:36:24 +0100

    changed Item

--- src/main/java/com/onlinestore/orderservice/model/Item.java
+++ src/main/java/com/onlinestore/orderservice/model/Item.java
@@ -1,9 +1,53 @@
 package com.onlinestore.orderservice.model;
 
-import lombok.Data;
+import java.util.Objects;
 
-@Data
 public class Item {
 	private String itemId;
 	private String itemName;
+
+	public Item() {
+	}
+
+	public Item(String itemId, String itemName) {
+		this.itemId = itemId;
+		this.itemName = itemName;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Item item = (Item) o;
+		return Objects.equals(itemId, item.itemId);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(itemId);
+	}
+
+	@Override
+	public String toString() {
+		return "Item{" +
+				"itemId='" + itemId + '\'' +
+				", itemName='" + itemName + '\'' +
+				'}';
+	}
 }
