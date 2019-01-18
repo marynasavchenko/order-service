65f1131a37dc43963405e0270c7daf3903f709e3
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-01-18 17:34:42 +0100

    changed ShoppingPosition

--- src/main/java/com/onlinestore/orderservice/model/ShoppingPosition.java
+++ src/main/java/com/onlinestore/orderservice/model/ShoppingPosition.java
@@ -1,11 +1,62 @@
 package com.onlinestore.orderservice.model;
 
-import lombok.Data;
+import java.math.BigDecimal;
 
-@Data
 public class ShoppingPosition {
 	private String shoppingPositionId;
 	private Item item;
-	private int amount;
+	private int quantity;
+	private BigDecimal price;
 
+	public ShoppingPosition() {
+	}
+
+	public ShoppingPosition(String shoppingPositionId, Item item, int quantity, BigDecimal price) {
+		this.shoppingPositionId = shoppingPositionId;
+		this.item = item;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public String getShoppingPositionId() {
+		return shoppingPositionId;
+	}
+
+	public void setShoppingPositionId(String shoppingPositionId) {
+		this.shoppingPositionId = shoppingPositionId;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "ShoppingPosition{" +
+				"shoppingPositionId='" + shoppingPositionId + '\'' +
+				", item=" + item +
+				", quantity=" + quantity +
+				", price=" + price +
+				'}';
+	}
 }
