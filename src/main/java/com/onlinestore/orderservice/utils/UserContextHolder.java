215817d8a99bed98deb8051e633f07fe766f4562
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-14 14:35:09 +0100

    added UserContextHolder

---
+++ src/main/java/com/onlinestore/orderservice/utils/UserContextHolder.java
@@ -0,0 +1,26 @@
+package com.onlinestore.orderservice.utils;
+
+import org.springframework.util.Assert;
+
+public class UserContextHolder {
+	private static final ThreadLocal<UserContext> threadLocalUserContext = new ThreadLocal<>();
+
+	public static final UserContext getContext() {
+		UserContext userContext = threadLocalUserContext.get();
+
+		if (userContext == null) {
+			userContext = createEmptyContext();
+			threadLocalUserContext.set(userContext);
+		}
+		return threadLocalUserContext.get();
+	}
+
+	public static final void setContext(UserContext userContext) {
+		Assert.notNull(userContext, "Only non-null UserContext instances are permitted");
+		threadLocalUserContext.set(userContext);
+	}
+
+	public static final UserContext createEmptyContext() {
+		return new UserContext();
+	}
+}
