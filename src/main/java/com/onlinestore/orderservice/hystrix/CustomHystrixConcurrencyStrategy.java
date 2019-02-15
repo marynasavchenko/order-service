94ce75d3b3cafff8d162faee7ccef2fd7113dc08
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-15 13:34:32 +0100

    added getBlockingQueue()

--- src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
@@ -2,11 +2,21 @@ package com.onlinestore.orderservice.hystrix;
 
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
 
+import java.util.concurrent.BlockingQueue;
+
 public class CustomHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
 	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
 
 	public CustomHystrixConcurrencyStrategy(HystrixConcurrencyStrategy existingConcurrencyStrategy) {
 		this.existingConcurrencyStrategy = existingConcurrencyStrategy;
+	}
+
+	@Override
+	public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize){
+		BlockingQueue<Runnable> blockingQueue;
+		if(existingConcurrencyStrategy!=null) blockingQueue=existingConcurrencyStrategy.getBlockingQueue(maxQueueSize);
+		else blockingQueue=super.getBlockingQueue(maxQueueSize);
+		return blockingQueue;
 	}
 
 }
