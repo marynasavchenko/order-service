e6ace0b0f44c9f9b8a413aa950fa0880a4d3a3fa
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-15 14:15:45 +0100

    added getThreadPool()

--- src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
@@ -1,10 +1,14 @@
 package com.onlinestore.orderservice.hystrix;
 
+import com.netflix.hystrix.HystrixThreadPoolKey;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class CustomHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
 	private HystrixConcurrencyStrategy existingConcurrencyStrategy;
@@ -21,8 +25,21 @@ public class CustomHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy
 
 	@Override
 	public <T> HystrixRequestVariable<T> getRequestVariable(HystrixRequestVariableLifecycle<T> requestVariableLifecycle) {
-		if (existingConcurrencyStrategy != null) return existingConcurrencyStrategy.getRequestVariable(requestVariableLifecycle);
+		if (existingConcurrencyStrategy != null)
+			return existingConcurrencyStrategy.getRequestVariable(requestVariableLifecycle);
 		return super.getRequestVariable(requestVariableLifecycle);
+	}
+
+	@Override
+	public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey,
+	                                        HystrixProperty<Integer> corePoolSize,
+	                                        HystrixProperty<Integer> maxPoolSize,
+	                                        HystrixProperty<Integer> keepAliveTime,
+	                                        TimeUnit unit,
+	                                        BlockingQueue<Runnable> workQueue) {
+		if (existingConcurrencyStrategy != null)
+			return existingConcurrencyStrategy.getThreadPool(threadPoolKey, corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue);
+		return super.getThreadPool(threadPoolKey, corePoolSize, maxPoolSize, keepAliveTime, unit, workQueue);
 	}
 
 }
