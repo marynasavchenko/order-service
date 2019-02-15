8cc80b517a71e48b48d6a0858b9ba217c3905eaa
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-15 13:57:28 +0100

    added getRequestVariable()

--- src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/CustomHystrixConcurrencyStrategy.java
@@ -1,6 +1,8 @@
 package com.onlinestore.orderservice.hystrix;
 
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
 
 import java.util.concurrent.BlockingQueue;
 
@@ -12,11 +14,15 @@ public class CustomHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy
 	}
 
 	@Override
-	public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize){
-		BlockingQueue<Runnable> blockingQueue;
-		if(existingConcurrencyStrategy!=null) blockingQueue=existingConcurrencyStrategy.getBlockingQueue(maxQueueSize);
-		else blockingQueue=super.getBlockingQueue(maxQueueSize);
-		return blockingQueue;
+	public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize) {
+		if (existingConcurrencyStrategy != null) return existingConcurrencyStrategy.getBlockingQueue(maxQueueSize);
+		return super.getBlockingQueue(maxQueueSize);
+	}
+
+	@Override
+	public <T> HystrixRequestVariable<T> getRequestVariable(HystrixRequestVariableLifecycle<T> requestVariableLifecycle) {
+		if (existingConcurrencyStrategy != null) return existingConcurrencyStrategy.getRequestVariable(requestVariableLifecycle);
+		return super.getRequestVariable(requestVariableLifecycle);
 	}
 
 }
