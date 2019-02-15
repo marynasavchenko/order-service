0afd8d3ba501da1ccd1f82f4000e094e76cd19b6
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-15 14:24:57 +0100

    added create()

--- src/main/java/com/onlinestore/orderservice/hystrix/DelegatingUserContextCallable.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/DelegatingUserContextCallable.java
@@ -1,10 +1,25 @@
 package com.onlinestore.orderservice.hystrix;
 
+import com.onlinestore.orderservice.utils.UserContext;
+
 import java.util.concurrent.Callable;
 
 public final class DelegatingUserContextCallable<V> implements Callable<V> {
+
+	private final Callable<V> delegate;
+	private UserContext originalUserContext;
+
+	public DelegatingUserContextCallable(Callable<V> delegate, UserContext userContext) {
+		this.delegate = delegate;
+		this.originalUserContext = userContext;
+	}
+
 	@Override
 	public V call() throws Exception {
 		return null;
+	}
+
+	public static <V> Callable<V> create(Callable<V> delegate, UserContext userContext) {
+		return new DelegatingUserContextCallable<V>(delegate, userContext);
 	}
 }
