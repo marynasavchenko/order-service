77796752877439cdab59d3372a04419488fc6caf
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-20 09:30:30 +0100

    implement intercept()

--- src/main/java/com/onlinestore/orderservice/utils/UserContextInterceptor.java
+++ src/main/java/com/onlinestore/orderservice/utils/UserContextInterceptor.java
@@ -1,5 +1,6 @@
 package com.onlinestore.orderservice.utils;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -8,8 +9,13 @@ import org.springframework.http.client.ClientHttpResponse;
 import java.io.IOException;
 
 public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-		return null;
+		HttpHeaders headers = request.getHeaders();
+		headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+		headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
+
+		return execution.execute(request, body);
 	}
 }
