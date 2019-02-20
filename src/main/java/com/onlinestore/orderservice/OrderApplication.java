d49558b07c1df6821ca053787c8015c8e8cde825
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-20 09:37:09 +0100

    added getRestTemplate()

--- src/main/java/com/onlinestore/orderservice/OrderApplication.java
+++ src/main/java/com/onlinestore/orderservice/OrderApplication.java
@@ -1,14 +1,36 @@
 package com.onlinestore.orderservice;
 
+import com.onlinestore.orderservice.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 public class OrderApplication {
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		List interceptors = restTemplate.getInterceptors();
+		if (interceptors == null) {
+			restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			restTemplate.setInterceptors(interceptors);
+		}
+		return restTemplate;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
