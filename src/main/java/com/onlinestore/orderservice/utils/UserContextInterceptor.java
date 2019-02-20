69aaf6e7876afef6560ffde3b29503f360dbf60a
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-02-20 09:29:09 +0100

    added UserContextInterceptor

---
+++ src/main/java/com/onlinestore/orderservice/utils/UserContextInterceptor.java
@@ -0,0 +1,15 @@
+package com.onlinestore.orderservice.utils;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class UserContextInterceptor implements ClientHttpRequestInterceptor {
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+		return null;
+	}
+}
