da983855c7af2cea3562df0418802fd432126614
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-15 10:53:17 +0100

    added package-info to hystrix

---
+++ src/main/java/com/onlinestore/orderservice/hystrix/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes for implementing custom HystrixConcurrencyStrategy.
+ * <p>
+ * Used to propagate the parent thread’s context to threads managed by the Hystrix Thread pool.
+ * <p/>
+ */
+package com.onlinestore.orderservice.hystrix;
