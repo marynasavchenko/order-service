5b23545a31d899bfa7b61b1da1b27ae2873dcba2
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-15 11:20:42 +0100

    added package-info contextinfoutils

---
+++ src/main/java/com/onlinestore/orderservice/contextinfoutils/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes that intersect calls, retrieve contextual information from HTTP requests and store it.
+ * <p>
+ * Used to make contextual information available anywhere in service call.
+ */
+package com.onlinestore.orderservice.contextinfoutils;
--- src/main/java/com/onlinestore/orderservice/hystrix/package-info.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/package-info.java
@@ -2,6 +2,5 @@
  * Classes for implementing custom HystrixConcurrencyStrategy.
  * <p>
  * Used to propagate the parent thread’s context to threads managed by the Hystrix Thread pool.
- * <p/>
  */
 package com.onlinestore.orderservice.hystrix;
