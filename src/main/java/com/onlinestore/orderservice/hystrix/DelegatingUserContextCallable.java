5fb234c4974eda64115b57705dc28feb6473b56f
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-20 10:38:21 +0100

    added doc comments to DelegatingUserContextCallable

--- src/main/java/com/onlinestore/orderservice/hystrix/DelegatingUserContextCallable.java
+++ src/main/java/com/onlinestore/orderservice/hystrix/DelegatingUserContextCallable.java
@@ -5,16 +5,42 @@ import com.onlinestore.orderservice.contextinfoutils.UserContextHolder;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Implementation of {@code Callable} interface.
+ * <p>
+ * Propagate thread context of the parent thread to Hystrix command.
+ *
+ * @param <V> the result type of method call
+ */
 public final class DelegatingUserContextCallable<V> implements Callable<V> {
-
+	/**
+	 * Callable that would be invoked by a thread managed by a Hystrix command pool.
+	 */
 	private final Callable<V> delegatedCallable;
+
+	/**
+	 * User context coming from parent thread that initiated the call.
+	 */
 	private UserContext originalUserContext;
 
+	/**
+	 * Constructs a new {@code DelegatingUserContextCallable} instance.
+	 *
+	 * @param delegatedCallable Callable that would be invoked by a thread
+	 * @param userContext       user context coming from parent thread
+	 */
 	public DelegatingUserContextCallable(Callable<V> delegatedCallable, UserContext userContext) {
 		this.delegatedCallable = delegatedCallable;
 		this.originalUserContext = userContext;
 	}
 
+	/**
+	 * Invoke the {@code call} method of the delegated Callable. This call invokes the method protected
+	 * by the @HystrixCommand annotation.
+	 *
+	 * @return result
+	 * @throws Exception
+	 */
 	@Override
 	public V call() throws Exception {
 		UserContextHolder.setContext(originalUserContext);
