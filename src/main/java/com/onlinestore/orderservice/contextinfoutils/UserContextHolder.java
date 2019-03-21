8e0f7b07b3741b11a1faef9cce3b31704b7baa25
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-21 10:21:07 +0100

    added doc comments to UserContextHolder

--- src/main/java/com/onlinestore/orderservice/contextinfoutils/UserContextHolder.java
+++ src/main/java/com/onlinestore/orderservice/contextinfoutils/UserContextHolder.java
@@ -2,9 +2,20 @@ package com.onlinestore.orderservice.contextinfoutils;
 
 import org.springframework.util.Assert;
 
+/**
+ * Class that used to store the UserContext in a ThreadLocal class.
+ */
 public class UserContextHolder {
+	/**
+	 * Contextual information {@code UserContext} saved in {@code ThreadLocal}.
+	 */
 	private static final ThreadLocal<UserContext> threadLocalUserContext = new ThreadLocal<>();
 
+	/**
+	 * Retrieves UserContext for consumption.
+	 *
+	 * @return contextual information from threadLocal
+	 */
 	public static final UserContext getContext() {
 		UserContext userContext = threadLocalUserContext.get();
 
@@ -15,11 +26,21 @@ public class UserContextHolder {
 		return threadLocalUserContext.get();
 	}
 
+	/**
+	 * Stores a UserContext object in a ThreadLocal variable specific to the thread being run.
+	 *
+	 * @param userContext contextual information
+	 */
 	public static final void setContext(UserContext userContext) {
 		Assert.notNull(userContext, "Only non-null UserContext instances are permitted");
 		threadLocalUserContext.set(userContext);
 	}
 
+	/**
+	 * Creates empty {@code UserContext}.
+	 *
+	 * @return empty {@code UserContext}
+	 */
 	public static final UserContext createEmptyContext() {
 		return new UserContext();
 	}
