ed68cbeac26d137a469271b417261d950fa7ce16
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-22 08:51:16 +0100

    added doc comments to Client

--- src/main/java/com/onlinestore/orderservice/clients/Client.java
+++ src/main/java/com/onlinestore/orderservice/clients/Client.java
@@ -2,6 +2,9 @@ package com.onlinestore.orderservice.clients;
 
 import com.onlinestore.orderservice.domain.Customer;
 
+/**
+ * {@link Client} defines an interface that supports operations for invoking customer service.
+ */
 public interface Client {
 	Customer getCustomer(String customerId);
 }
