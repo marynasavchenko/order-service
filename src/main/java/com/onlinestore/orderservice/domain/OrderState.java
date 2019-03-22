39a3f1ace766beb059acbfcd238b714b12eb8f26
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-03-22 09:36:22 +0100

    added doc comments to OrderState

--- src/main/java/com/onlinestore/orderservice/domain/OrderState.java
+++ src/main/java/com/onlinestore/orderservice/domain/OrderState.java
@@ -1,9 +1,32 @@
 package com.onlinestore.orderservice.domain;
 
+/**
+ * Order states that can be used
+ * <li>{@link #ACCEPTANCE_PENDING}</li>
+ * <li>{@link #ACCEPTED}</li>
+ * <li>{@link #REJECTED}</li>
+ * <li>{@link #CANCEL_PENDING}</li>
+ * <li>{@link #CANCELLED}</li>
+ */
 public enum OrderState {
+	/**
+	 * State for order waiting to be accepted.
+	 */
 	ACCEPTANCE_PENDING,
+	/**
+	 * State for accepted order.
+	 */
 	ACCEPTED,
+	/**
+	 * State for rejected order.
+	 */
 	REJECTED,
+	/**
+	 * State for order waiting to be canceled.
+	 */
 	CANCEL_PENDING,
-	CANCELLED,
+	/**
+	 * State for canceled order.
+	 */
+	CANCELLED
 }
