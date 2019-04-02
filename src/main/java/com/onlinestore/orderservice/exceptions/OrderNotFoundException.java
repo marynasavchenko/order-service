1cd3364eed04e5696101d5698290954b1b94a1d9
marinasavchenko <msavchenko.abacuspro@gmail.com>
2019-04-02 11:31:27 +0200

    added doc comments to OrderNotFoundException

--- src/main/java/com/onlinestore/orderservice/exceptions/OrderNotFoundException.java
+++ src/main/java/com/onlinestore/orderservice/exceptions/OrderNotFoundException.java
@@ -4,6 +4,11 @@ package com.onlinestore.orderservice.exceptions;
  * Exception thrown when order can not be found.
  */
 public class OrderNotFoundException extends RuntimeException {
+	/**
+	 * Constructs a new {@code OrderNotFoundException} instance.
+	 *
+	 * @param orderId unique id of the order
+	 */
 	public OrderNotFoundException(String orderId) {
 		super("Order not found" + orderId);
 	}
